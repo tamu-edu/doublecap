d35e4cf5d85c91181ae12e4e6641ddd08face9bd
AJ Biffl <ajbiffl219@gmail.com>
2024-12-13 09:33:29 -0600

    stopped tracking build dir

--- build/regen_figs.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-./doublecap list-volumes.mac > volume_list.txt
-./doublecap visbox.mac
-./doublecap vis.mac
-./doublecap vis100.mac
--- build/src/CMakeFiles/doublecap.dir/DCPhysics.cc.o.d
+++
@@ -1,2 +0,0 @@
-src/CMakeFiles/doublecap.dir/DCPhysics.cc.o: \
-  /Users/ajbiffl/Documents/CDMS/defects/doublecap/src/DCPhysics.cc
