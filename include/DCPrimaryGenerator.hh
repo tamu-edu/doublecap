beaae3d717d52b253b90b8e6f1be3a76c3b8de98
AJ Biffl <ajbiffl219@gmail.com>
2024-12-16 14:05:29 -0600

    new TestGenerator for thermal neutrons in detector volume
    separated out generator classes into their own file

---
+++ include/DCPrimaryGenerator.hh
@@ -0,0 +1,42 @@
+#ifndef DCPRIMARYGENERATOR_HH
+#define DCPRIMARYGENERATOR_HH
+
+#include "G4VUserPrimaryGeneratorAction.hh"
+#include "G4Event.hh"
+#include "G4ParticleGun.hh"
+#include "G4Ions.hh"
+#include "G4SystemOfUnits.hh"
+
+
+class DCPrimaryGenerator : public G4VUserPrimaryGeneratorAction {
+public:
+    DCPrimaryGenerator(G4int, G4int, G4ThreeVector);
+    ~DCPrimaryGenerator() {;}
+    void GeneratePrimaries(G4Event *);
+    void GetParticleDefinition();
+    void SetParticlePositionMomentum();
+private:
+    G4double sideLength = 1.*mm; // side length of source cube
+    G4ParticleGun *gun;
+    G4ParticleDefinition *theParticle;
+    G4int Z;
+    G4int A;
+    G4ThreeVector source_position;
+};
+
+
+class TestGenerator : public G4VUserPrimaryGeneratorAction {
+public:
+    TestGenerator();
+    ~TestGenerator() {;}
+    void GeneratePrimaries(G4Event *);
+private:
+    G4ParticleGun *gun;
+    G4String pname = "neutron";
+    G4double penergy = 5.*eV;
+};
+
+
+
+
+#endif
---
+++ src/DCPrimaryGenerator.cc
@@ -0,0 +1,84 @@
+
+
+
+#include "DCUtilities.hh"
+#include "DCPrimaryGenerator.hh"
+#include "DCRunAction.hh"
+#include "DCEventAction.hh"
+
+#include "G4ThreeVector.hh"
+#include "G4SystemOfUnits.hh"
+#include "G4IonTable.hh"
+#include "G4ParticleTable.hh"
+
+
+DCPrimaryGenerator::DCPrimaryGenerator(G4int Z, G4int A, G4ThreeVector source_position) : 
+    G4VUserPrimaryGeneratorAction(), 
+    gun(nullptr), 
+    theParticle(nullptr) , 
+    Z(Z), A(A),
+    source_position(source_position)
+{
+    gun = new G4ParticleGun(1);
+}
+
+
+void DCPrimaryGenerator::GetParticleDefinition() {
+    // define particle type for gun and 
+    if (Z == 0 && A == 0) {
+        gun->SetParticleEnergy(1.*eV);
+        theParticle = G4ParticleTable::GetParticleTable()->FindParticle("neutron");
+    } else {
+        gun->SetParticleEnergy(0.);
+        theParticle = G4IonTable::GetIonTable()->GetIon(Z, A);
+    }
+    gun->SetParticleDefinition(theParticle);
+}
+
+void DCPrimaryGenerator::SetParticlePositionMomentum() {
+
+    gun->SetParticlePosition(source_position + 
+        G4ThreeVector((G4UniformRand() - 0.5)*sideLength,
+                      (G4UniformRand() - 0.5)*sideLength,
+                      (G4UniformRand() - 0.5)*sideLength
+                      
+    ));
+
+    if (Z == 0 && A == 0) { // particle = neutron: randomize direction
+            
+        gun->SetParticleMomentumDirection(RandomDirection()); // momentum direction (magnitude set by particle enery)
+    }
+}
+
+void DCPrimaryGenerator::GeneratePrimaries(G4Event *anEvent) {
+    if (!theParticle) {
+        GetParticleDefinition();
+    }
+    SetParticlePositionMomentum();
+    gun->GeneratePrimaryVertex(anEvent);
+}
+
+
+
+
+TestGenerator::TestGenerator() : G4VUserPrimaryGeneratorAction(), gun(nullptr) 
+{
+    gun = new G4ParticleGun(1);
+    gun->SetParticleDefinition(
+        G4ParticleTable::GetParticleTable()->FindParticle(pname)
+    );
+    gun->SetParticlePosition(G4ThreeVector(0., 0., 1.5*cm));
+    gun->SetParticleMomentumDirection(G4ThreeVector(1.,0.,0.));
+    gun->SetParticleEnergy(penergy);
+}
+
+
+void TestGenerator::GeneratePrimaries(G4Event *anEvent) {
+    gun->GeneratePrimaryVertex(anEvent);
+}
+
+
+
+
+
+
