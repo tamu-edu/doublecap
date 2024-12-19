49464352d6de3ce97f5c0798d408f5427dabbba0
AJ Biffl <ajbiffl219@gmail.com>
2024-12-19 12:07:44 -0600

    minor changes, nothing implemented

--- src/DCStackingAction.cc
+++ src/DCStackingAction.cc
@@ -6,6 +6,8 @@
 
 
 G4ClassificationOfNewTrack DCStackingAction::ClassifyNewTrack(const G4Track *track) {
-
-    return fUrgent; // return normal "add-to-top-of-stack" status unless we have a reason to kill
+    G4ClassificationOfNewTrack status = fUrgent;
+    const G4ParticleDefinition *part = track->GetParticleDefinition();
+    
+    return status; // return normal "add-to-top-of-stack" status unless we have a reason to kill
 }
