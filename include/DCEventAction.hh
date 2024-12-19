83f0c1c05a129883dbd6aa8aecf8e15161803462
AJ Biffl <ajbiffl219@gmail.com>
2024-12-19 12:08:14 -0600

    added DCTrackAction memeber

--- include/DCEventAction.hh
+++ include/DCEventAction.hh
@@ -4,12 +4,13 @@
 #include "G4UserEventAction.hh"
 
 #include "DCRunAction.hh"
+#include "DCTrackAction.hh"
 
 class DCRunAction;
 
 class DCEventAction : public G4UserEventAction {
 public:
-    DCEventAction(DCRunAction*);
+    DCEventAction(DCRunAction*, DCTrackAction*);
     virtual ~DCEventAction() override = default;
 
     void BeginOfEventAction(const G4Event*) override;
@@ -17,6 +18,7 @@ public:
 
 private:
     DCRunAction *runAction;
+    DCTrackAction *trackAction;
 
     G4int id_lm = -1;
     G4int id_hm = -1;
