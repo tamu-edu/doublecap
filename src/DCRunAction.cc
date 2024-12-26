d57300cd322d53ec85b19de2d898f4bd1e0854ce
AJ Biffl <ajbiffl219@gmail.com>
2024-12-26 15:10:43 -0700

    adding timestamp to output file
    (attempted) switch to MT

--- src/DCRunAction.cc
+++ src/DCRunAction.cc
@@ -7,7 +7,7 @@
 #include "G4AccumulableManager.hh"
 #include "G4ParticleGun.hh"
 #include "G4Run.hh"
-#include "G4RunManager.hh"
+#include "G4MTRunManager.hh"
 
 
 DCRunAction::DCRunAction() {
@@ -19,47 +19,77 @@ DCRunAction::DCRunAction() {
 
 
 void DCRunAction::BeginOfRunAction(const G4Run *run) {
+
+    if (verbosity > 0) {
+        G4cout << "Start of BeginOfRunAction" << G4endl;
+    }
     G4AccumulableManager *accumblmgr = G4AccumulableManager::Instance();
     accumblmgr->Reset();
 
     // inform the runManager to save random number seed
-    G4RunManager::GetRunManager()->SetRandomNumberStore(false);
+    G4MTRunManager::GetRunManager()->SetRandomNumberStore(false);
 
 
     auto analysismgr = G4RootAnalysisManager::Instance();
     //analysismgr->SetNtupleMerging(true);
-    G4String filename = "highmass.root";
-    analysismgr->OpenFile(filename);
 
-    G4int id = analysismgr->CreateNtuple("tree", "tree");
-    analysismgr->CreateNtupleIColumn("EventNum");
-    analysismgr->CreateNtupleIColumn("TrkNum");
-    analysismgr->CreateNtupleDColumn("Edep");
-    analysismgr->CreateNtupleSColumn("VolName");
-    analysismgr->CreateNtupleSColumn("PName");
-    analysismgr->CreateNtupleSColumn("ProcName");
-    analysismgr->CreateNtupleSColumn("ParentVol");
-    analysismgr->CreateNtupleIColumn("IsCapture");
-    analysismgr->FinishNtuple();
+    if (1){//G4Threading::IsMasterThread()) {
+
+        analysismgr->SetNtupleMerging(true);
+
+        time_t now = time(0);
+        tm* localTime = localtime(&now);
+
+        char buffer[80];
+        strftime(buffer, sizeof(buffer), "%Y%m%d_%H%M%S", localTime);
+
+        G4String filename = "simdata_" + G4String(buffer) + ".root";
+        analysismgr->OpenFile(filename);
+
+        G4int id = analysismgr->CreateNtuple("tree", "tree");
+        analysismgr->CreateNtupleIColumn("EventNum");
+        analysismgr->CreateNtupleIColumn("TrkNum");
+        analysismgr->CreateNtupleDColumn("Edep");
+        analysismgr->CreateNtupleSColumn("VolName");
+        analysismgr->CreateNtupleSColumn("PName");
+        analysismgr->CreateNtupleSColumn("ProcName");
+        analysismgr->CreateNtupleSColumn("ParentVol");
+        analysismgr->CreateNtupleIColumn("IsCapture");
+        analysismgr->FinishNtuple();
+
+    }
 
     //G4cout << "id = " << id << " out of " << analysismgr->GetNofNtuples() << G4endl;
     //G4cout << "GetNofNtuple = " << analysismgr->GetNofNtuples() << G4endl;
-    //G4cout << "finished BeginOfRunAction" << G4endl;
+    //G4cout << "finished BeginOfRunAction" << G4endl
+    if (verbosity > 0) {
+        G4cout << "End of BeginOfRunAction" << G4endl;
+    }
 }
 
 
 void DCRunAction::EndOfRunAction(const G4Run *run) {
+
+    if (verbosity > 0) {
+        G4cout << "Start of EndOfRunAction" << G4endl;
+    }
+
     G4int nofEvents = run->GetNumberOfEvent();
     if (nofEvents == 0) return;
 
     G4AccumulableManager *accumblmgr = G4AccumulableManager::Instance();
     accumblmgr->Merge();
 
-    auto analysismgr = G4RootAnalysisManager::Instance();
-    analysismgr->Write();
-    analysismgr->CloseFile();
+    if (G4Threading::IsMasterThread()) {
+        auto analysismgr = G4RootAnalysisManager::Instance();
+        analysismgr->Write();
+        analysismgr->CloseFile();
+    }
 
     //G4cout << "GetNofNtuple = " << analysismgr->GetNofNtuples() << G4endl;
+    if (verbosity > 0) {
+        G4cout << "End of EndOfRunAction" << G4endl;
+    }
 
 }
 
