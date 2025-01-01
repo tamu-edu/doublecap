0488036a96e1a9d477df3fdf6297c53a73a197ea
AJ Biffl <ajbiffl219@gmail.com>
2025-01-01 12:14:43 -0600

    output error messages to log files as well

--- macros/repeat_production_run.sh
+++ macros/repeat_production_run.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
-N=10
+N=40
 echo "starting $N repetitions"
 start=$SECONDS
 for ((n=1;n<=$N;n++)); do
     echo "starting $n / $N"
     s1=$SECONDS
-    ./doublecap prod.mac > "prod_output_$n.txt"
+    ./doublecap prod.mac &> "prod_output_$n.txt"
     duration=$(( SECONDS - s1))
     echo "finished: $duration seconds"
     sleep 10
