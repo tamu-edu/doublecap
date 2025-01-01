c1410ddefee532e7dd3aa8e1000a10dc8152083d
AJ Biffl <ajbiffl219@gmail.com>
2024-12-31 20:33:26 -0600

    slight change to output

--- macros/repeat_production_run.sh
+++ macros/repeat_production_run.sh
@@ -1,6 +1,15 @@
 #! /bin/bash
 N=10
+echo "starting $N repetitions"
+start=$SECONDS
 for ((n=1;n<=$N;n++)); do
+    echo "starting $n / $N"
+    s1=$SECONDS
     ./doublecap prod.mac > "prod_output_$n.txt"
-    echo $n / $N
+    duration=$(( SECONDS - s1))
+    echo "finished: $duration seconds"
+    sleep 10
 done
+
+duration=$(( SECONDS - start))
+echo "$duration seconds total"
