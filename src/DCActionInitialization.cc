c44b01941cc6579389287915b92c75b3a7d1ca74
AJ Biffl <ajbiffl219@gmail.com>
2025-02-06 09:00:17 -0600

    added flag to add DCStackingAction to kill gammas

--- src/DCActionInitialization.cc
+++ src/DCActionInitialization.cc
@@ -5,6 +5,7 @@
 #include "DCPrimaryGenerator.hh"
 #include "DCRunAction.hh"
 #include "DCTrackAction.hh"
+#include "DCStackingAction.hh"
 
 #include "G4ThreeVector.hh"
 #include "G4SystemOfUnits.hh"
@@ -12,10 +13,10 @@
 #include "G4ParticleTable.hh"
 
 
-DCInitialization::DCInitialization(G4int Z, G4int A, DCGeometry *geom, G4int mode, G4String fname) : 
+DCInitialization::DCInitialization(G4int Z, G4int A, DCGeometry *geom, G4int mode, G4String fname, G4bool killGammas) : 
     G4VUserActionInitialization(), 
     Z(Z), A(A), mode(mode), filename(fname),
-    geometry(geom)
+    geometry(geom), fKillGammas(killGammas)
 {;}
 
 void DCInitialization::BuildForMaster() const {
@@ -27,6 +28,8 @@ void DCInitialization::Build() const {
     DCRunAction *runAction = new DCRunAction(filename);
     SetUserAction(runAction);
     SetUserAction(new DCTrackAction);
+
+    if (fKillGammas) SetUserAction(new DCStackingAction);
 
     switch (mode) {
         case 0:
