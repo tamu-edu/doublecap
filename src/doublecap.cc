ddee88676abee3caf74a907997e5c988b4c1a358
AJ Biffl <ajbiffl219@gmail.com>
2025-02-17 16:48:21 -0600

    completely revamped command line arguments to specify geometry, simulation mode, and run settings from command line

--- src/doublecap.cc
+++ src/doublecap.cc
@@ -39,25 +39,113 @@
 #include "DCSensitiveDetector.hh"
 
 
-const G4bool RANDOMIZE = true; // set random seed for RNG
-const G4int NUMBEROFTHREADS = 8; // number of threads for MT mode
-
-// simulation mode:
-// 0 = test source (0.1 eV neutrons at (0,0,0))
-// 1 = rate simulation (Cf source)
-// 2 = capture simulation (capture source)
-const G4int SIMULATIONMODE = 1; 
-
-
 int main(int argc, char *argv[]) {
+    /*
+    usage: ./doublecap macro.mac [args]
 
-    auto start = std::chrono::steady_clock::now();
+    command line arguments
 
-    auto *runmgr = G4RunManagerFactory::CreateRunManager();
-    runmgr->SetNumberOfThreads(NUMBEROFTHREADS);
-    G4PhysListFactory *factory = new G4PhysListFactory;
-    G4VisManager* vismgr = new G4VisExecutive;
+    -i  idx
+        ID or index number of filename, appended after timestamp to denote multiple runs started at the same time
 
+    -k kill-gammas
+        integer flag to specify whether or not to kill gammas, e-, and e+ (default 0)
+
+    -l Pb-thickness
+        thickness (cm) of lead shield between source and detector box (default 5 cm)
+
+    -m  /macro/path
+        Path to macro files (all macro files should be given just as titles and this argument should be specified)
+
+    -n  number-of-threads
+        specify number of threads for multithreading (default 8)
+
+    -o  /output/dir
+        Path to output directory, e.g., for simulation mode zero, output file names will be "/output/dir/test_data/testsim_XXXXXX_XXXXXX_XXXX_tX.root"
+
+    -p PE-thickness
+        thickness (cm) of polyethylene between source and detector box (default 10 cm)
+
+    -r  randomize
+        integer flag, whether or not to set random state with new random seed (default 1)
+
+    -s  simulation-mode
+        set simulation mode (default 1)
+        - 0 = test source (0.1 eV neutrons at (0,0,0))
+        - 1 = rate simulation (Cf source)
+        - 2 = capture simulation (capture source)
+    */
+    
+    G4String argval;
+    G4String filename;
+    G4String index = "";
+    G4bool add_macro_path = false;
+    G4String macro_path;
+    G4String output_dir = "";
+    G4bool KILLGAMMAS = false;
+    G4bool RANDOMIZE = true;
+    G4int NUMBEROFTHREADS = 8;
+    G4int SIMULATIONMODE = 1; 
+    G4double leadthickness = 10.*cm; // thickness (height) of lead shield
+    G4double pethickness = 5.*cm; // thickness of polyethylene between detector box and lead shield
+
+    if (argc > 2) { // extra command line arguments
+        G4int arg_idx = 2;
+
+        while (arg_idx < argc) {
+
+            if (argv[arg_idx][0] == '-') {
+
+                argval = G4String(argv[arg_idx+1]);
+
+                switch (argv[arg_idx][1]) {
+                    case 'o': // /output/dir
+                    output_dir = argval + "/";
+                        break;
+
+                    case 'i': // idx
+                        index = G4String("_") + argval;
+                        break;
+                        
+                    case 'm': // /macro/path
+                        add_macro_path = true;
+                        macro_path = argval;
+                        break;
+
+                    case 'l': // Pb-thickness
+                        leadthickness = atof(argv[arg_idx+1])*cm;
+                        break;
+
+                    case 'p': // PE-thickness
+                        pethickness = atof(argv[arg_idx+1])*cm;
+                        break;
+
+                    case 'k': // kill-gammas
+                        KILLGAMMAS = (argval != "0");
+                        break;
+
+                    case 'n': //number-of-threads
+                        NUMBEROFTHREADS = atoi(argv[arg_idx+1]);
+                        break;
+
+                    case 'r': // randomize
+                        RANDOMIZE = (argval != "0");
+                        break;
+
+                    case 's': // simulation-mode
+                        SIMULATIONMODE = atoi(argv[arg_idx+1]);
+                        break;
+
+                    default:
+                        G4cout << "Unrecognized command-line option" << '"' << argv[arg_idx] << '"' << G4endl;
+                }
+                ++arg_idx;
+            } else {
+                G4cout << "Unused out-of-phase command line argument: \"" << argv[arg_idx] << '"' << G4endl;
+            }
+            ++arg_idx;
+        }
+    }
 
     if (RANDOMIZE) {
         auto SEED = time(nullptr);
@@ -67,30 +155,33 @@ int main(int argc, char *argv[]) {
         G4cout << "NOT RANDOMIZING" << G4endl;
     }
 
+    G4cout << "KILLGAMMAS = " << KILLGAMMAS << G4endl;
+    G4cout << "output_dir: " << output_dir << G4endl;
+    G4cout << "index: " << index << G4endl;
+    G4cout << "macro_path: " << macro_path << G4endl;
+    G4cout << "leadthickness = " << leadthickness/cm << " cm" << G4endl; 
+    G4cout << "pethickness = " << pethickness/cm << " cm" << G4endl; 
+
+    G4String title_prefix;
+    
     time_t now = time(0);
     tm* localTime = localtime(&now);
 
     char buffer[80];
     strftime(buffer, sizeof(buffer), "%Y%m%d_%H%M%S", localTime);
 
-    G4String filename;
-    G4String index = "";
-
-    if (argc > 2) {
-        index = G4String("_") + G4String(argv[2]);
-    }
-    
     switch (SIMULATIONMODE) {
         case 0:
-            filename = "test_data/testsim_" + G4String(buffer) + index + ".root";
+            filename = output_dir + "test_data/testsim_" + G4String(buffer) + index + ".root";
             G4cout << "SIMULATION MODE 0: TEST" << G4endl;
             break;
         case 1:
-            filename = "/scratch/user/ajbiffl3/doublecap/data/rate_sim/simdata_" + G4String(buffer) + index + ".root";
+            title_prefix = G4String("Lead") + G4String(to_string(G4int(leadthickness/cm))) + G4String("_PE") + G4String(to_string(G4int(pethickness/cm))) + G4String("_");
+            filename = output_dir + "shield_sim/" + title_prefix + G4String(buffer) + index + ".root";
             G4cout << "SIMULATION MODE 1: RATE SIMULATION" << G4endl;
             break;
         case 2:
-            filename = "capture_sim/simdata_" + G4String(buffer) + index + ".root";
+            filename = output_dir + "capture_sim/simdata_" + G4String(buffer) + index + ".root";
             G4cout << "SIMULATION MODE 2: CAPTURE SIMULATION" << G4endl;
             break;
         default:
@@ -98,6 +189,11 @@ int main(int argc, char *argv[]) {
             return 1;
     }
     G4cout << "Writing data to " << filename << G4endl;
+
+    auto *runmgr = G4RunManagerFactory::CreateRunManager();
+    runmgr->SetNumberOfThreads(NUMBEROFTHREADS);
+    G4PhysListFactory *factory = new G4PhysListFactory;
+    G4VisManager* vismgr = new G4VisExecutive;
 
     // geometry
     G4double lm_face = 1.*cm; // width of lowmass
@@ -110,8 +206,6 @@ int main(int argc, char *argv[]) {
     G4double cu_thick = 0.2*mm; // thickness of copper boxes
     G4double spacing = 1.5*mm; // spacing between silicon detectors
 
-    G4double pethickness = 5.*cm; // thickness of polyethylene between detector box and lead shield
-    G4double leadthickness = 12.*cm; // thickness (height) of lead shield
     G4double leadradius = 3.*cm; // radius of cylindrical lead shield
     G4double sourcesize = 1.*mm; // side length of Cf cube
 
@@ -126,7 +220,7 @@ int main(int argc, char *argv[]) {
     G4cout << "sourcez = " << sourcez/cm << " cm" << G4endl;
 
     runmgr->SetUserInitialization(factory->GetReferencePhysList("Shielding_HPT"));
-    runmgr->SetUserInitialization(new DCInitialization(Z, A, geometry, SIMULATIONMODE, filename));
+    runmgr->SetUserInitialization(new DCInitialization(Z, A, geometry, SIMULATIONMODE, filename, KILLGAMMAS));
     runmgr->SetUserInitialization(geometry);
 
     runmgr->Initialize();
@@ -139,15 +233,19 @@ int main(int argc, char *argv[]) {
     // set to get all radioactive decays
     G4HadronicParameters::Instance()->SetTimeThresholdForRadioactiveDecay(1.0e+6*year);
 
-
+    // measure execution time not initialization time
+    auto start = std::chrono::steady_clock::now();
+    
     G4UImanager *UImanager = G4UImanager::GetUIpointer();
+    if (add_macro_path) {
+        G4String command = "/control/macroPath ";
+        UImanager->ApplyCommand(command + macro_path);
+    }
     if (argc > 1) {
         G4String macro = argv[1];
         G4String command = "/control/execute ";
         UImanager->ApplyCommand(command + macro);
-    } else { // no macro - return error
-        //G4cout << "No macro supplied. Executing test_macro.mac" << G4endl;
-        //UImanager->ApplyCommand("/control/execute test_macro.mac");
+    } else { // no macro - interactive mode
         G4UIExecutive *ui = new G4UIExecutive(argc, argv);
         ui->SessionStart();
         delete ui;
